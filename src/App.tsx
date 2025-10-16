84abb8e64caf270a31e65c50670b8c0f3d86ad3c
godcl1623 <godcl1623@gmail.com>
2025-10-16 22:37:42 +0900

    feat: 스타일 정돈

--- src/App.tsx
+++ src/App.tsx
@@ -6,55 +6,44 @@ import {
   Settings,
 } from "lucide-react";
 
+import Button from "@/shared/components/atoms/button.tsx";
+import type { BasicComponentProps } from "@/shared/types";
+
 function App() {
   return (
     <>
-      <header className={"flex items-center justify-between px-10 py-3 shadow"}>
-        <div className={"flex items-center gap-2"}>
+      <header className={"flex-center-between px-10 py-3 shadow"}>
+        <div className={"flex-center gap-2"}>
           <div className={"rounded-md bg-blue-600 p-1.5 text-white"}>
             <Bookmark />
           </div>
           <h1 className={"text-xl font-bold"}>LinkVault</h1>
         </div>
-        <div className={"flex items-center gap-4"}>
-          <button>
+        <div className={"flex-center gap-2"}>
+          <Button size={"icon-sm"} variant={"ghost"}>
             <Search className={"size-5"} />
-          </button>
-          <button>
+          </Button>
+          <Button size={"icon-sm"} variant={"ghost"}>
             <Settings className={"size-5"} />
-          </button>
-          <ul
-            className={"flex items-center gap-1 rounded-md bg-neutral-200 p-1"}
-          >
-            <li className={"flex items-center"}>
-              <button className={"rounded-sm bg-white p-1.5"}>
+          </Button>
+          <ul className={"flex-center gap-1 rounded-md bg-neutral-200 p-1"}>
+            <li className={"flex-center"}>
+              <Button size={"icon-sm"} variant={"outline"}>
                 <LayoutGrid className={"size-4 text-blue-600"} />
-              </button>
+              </Button>
             </li>
-            <li className={"flex items-center"}>
-              <button className={"rounded-sm bg-neutral-200 p-1.5"}>
+            <li className={"flex-center"}>
+              <Button size={"icon-sm"} variant={"ghost"}>
                 <LayoutList className={"size-4"} />
-              </button>
+              </Button>
             </li>
           </ul>
         </div>
       </header>
       <div className={"flex h-[calc(100vh-60px)]"}>
-        <aside className={"flex w-[15%] flex-col gap-5 p-5"}>
-          <button
-            className={
-              "rounded-sm border border-neutral-200 px-5 py-0.5 text-left text-sm font-bold"
-            }
-          >
-            All
-          </button>
-          <button
-            className={
-              "rounded-sm border border-neutral-200 px-5 py-0.5 text-left text-sm font-bold"
-            }
-          >
-            Favorites
-          </button>
+        <aside className={"flex w-[15%] flex-col gap-5 bg-slate-200/75 p-5"}>
+          <DefaultFilterButton>All</DefaultFilterButton>
+          <DefaultFilterButton>Favorites</DefaultFilterButton>
           <nav>
             <ul className={"flex flex-col gap-2"}>
               <li>
@@ -76,3 +65,11 @@ function App() {
 }
 
 export default App;
+
+function DefaultFilterButton({ children }: BasicComponentProps) {
+  return (
+    <Button size={"sm"} variant={"outline"} className={"text-sm font-bold"}>
+      {children}
+    </Button>
+  );
+}
