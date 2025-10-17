a4a43855f4d9c5f25f6a09252d4f0f19d4bbf8ec
godcl1623 <godcl1623@gmail.com>
2025-10-17 23:16:28 +0900

    style: 추상화 정도 수정

--- src/features/BookmarkList/BlankFallback.tsx
+++ src/features/BookmarkList/BlankFallback.tsx
@@ -4,23 +4,20 @@ import Button from "@/shared/components/atoms/button.tsx";
 
 export default function BlankFallback() {
   return (
-    <article className={STYLES.container}>
-      <div className={STYLES.icon}>
+    <article className={"flex-col-center-center size-full gap-5"}>
+      <div className={"rounded-full bg-blue-100 p-7 text-blue-500"}>
         <Bookmark className={"size-10"} />
       </div>
       <h2>No bookmarks yet</h2>
       <p className={"text-muted-foreground"}>
         Start building your collection by adding your first bookmark
       </p>
-      <Button variant={"blank"} className={STYLES.button}>
+      <Button
+        variant={"blank"}
+        className={"bg-blue-500 text-white hover:bg-blue-700"}
+      >
         Add Bookmark
       </Button>
     </article>
   );
 }
-
-const STYLES = {
-  container: "flex-col-center-center size-full gap-5",
-  icon: "rounded-full bg-blue-100 p-7 text-blue-500",
-  button: "bg-blue-500 text-white hover:bg-blue-700",
-};
--- src/features/Header/index.tsx
+++ src/features/Header/index.tsx
@@ -7,36 +7,58 @@ import {
 } from "lucide-react";
 
 import Button from "@/shared/components/atoms/button.tsx";
+import type { BasicComponentProps } from "@/shared/types";
 
 export default function Header() {
   return (
     <header className={"flex-center-between px-10 py-3 shadow"}>
-      <div className={"flex-center gap-2"}>
+      <div className={STYLES.container}>
         <div className={"rounded-md bg-blue-600 p-1.5 text-white"}>
           <Bookmark />
         </div>
         <h1>LinkVault</h1>
       </div>
-      <div className={"flex-center gap-2"}>
-        <Button size={"icon-sm"} variant={"ghost"}>
-          <Search className={"size-5"} />
-        </Button>
-        <Button size={"icon-sm"} variant={"ghost"}>
-          <Settings className={"size-5"} />
-        </Button>
+      <div className={STYLES.container}>
+        <OptionButton>
+          <Search className={STYLES.iconMd} />
+        </OptionButton>
+        <OptionButton>
+          <Settings className={STYLES.iconMd} />
+        </OptionButton>
         <ul className={"flex-center gap-1 rounded-md bg-neutral-200 p-1"}>
           <li className={"flex-center"}>
-            <Button size={"icon-sm"} variant={"outline"}>
-              <LayoutGrid className={"size-4 text-blue-600"} />
-            </Button>
+            <OptionButton isActive>
+              <LayoutGrid className={STYLES.iconSm} />
+            </OptionButton>
           </li>
           <li className={"flex-center"}>
-            <Button size={"icon-sm"} variant={"ghost"}>
-              <LayoutList className={"size-4"} />
-            </Button>
+            <OptionButton>
+              <LayoutList className={STYLES.iconSm} />
+            </OptionButton>
           </li>
         </ul>
       </div>
     </header>
+  );
+}
+
+const STYLES = {
+  container: "flex-center gap-2",
+  iconMd: "size-5",
+  iconSm: "size-4",
+};
+
+interface OptionButtonProps extends BasicComponentProps {
+  isActive?: boolean;
+}
+
+function OptionButton({ children, isActive = false }: OptionButtonProps) {
+  const activeVariant = isActive ? "outline" : "ghost";
+  const activeColor = isActive ? "text-blue-600" : "";
+
+  return (
+    <Button size={"icon-sm"} variant={activeVariant}>
+      <span className={activeColor}>{children}</span>
+    </Button>
   );
 }
