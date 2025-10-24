d296ffe4a05f9995eaa48ca736f48d4916bf7dfc
godcl1623 <godcl1623@gmail.com>
2025-10-24 23:01:42 +0900

    feat: 북마크 카드 기본 마크업

--- src/features/BookmarkList/index.tsx
+++ src/features/BookmarkList/index.tsx
@@ -1,11 +1,16 @@
+import { EllipsisVertical, Share2, Star } from "lucide-react";
+import type { MouseEvent } from "react";
+
 import BlankFallback from "@/features/BookmarkList/components/BlankFallback";
-import TagItem from "@/shared/components/molecules/TagItem.tsx";
+import Button from "@/shared/components/atoms/button";
+import TagItem from "@/shared/components/molecules/TagItem";
 import {
   Card,
   CardContent,
   CardDescription,
   CardTitle,
-} from "@/shared/components/organisms/card.tsx";
+} from "@/shared/components/organisms/card";
+import type { BasicComponentProps } from "@/shared/types";
 
 export default function BookmarkList() {
   const dummyList = ["foo"];
@@ -14,16 +19,22 @@ export default function BookmarkList() {
     <main className={"w-[85%] bg-blue-50/75 p-5"}>
       {dummyList.length === 0 && <BlankFallback />}
       <div>
-        <Card className={"h-[360px] w-[320px] gap-0 p-0"}>
+        <Card
+          className={
+            "h-[calc(50vh-64px)] w-[320px] cursor-pointer gap-0 p-0 hover:shadow-md active:[&:not(:has(button:active))]:brightness-90"
+          }
+        >
           <div className={"h-1/2 w-full rounded-t-xl bg-neutral-100"} />
-          <CardContent className={"p-3"}>
-            <header className={"mb-5 py-2"}>
+          <CardContent className={"h-1/2 px-5 py-3"}>
+            <header className={"py-2"}>
               <CardTitle className={"line-clamp-1 text-left"}>
                 Test Title
               </CardTitle>
             </header>
-            <CardDescription>Test Description</CardDescription>
-            <ul className={"my-5 flex gap-2"}>
+            <CardDescription className={"line-clamp-2 h-10"}>
+              Test Description
+            </CardDescription>
+            <ul className={"my-3 line-clamp-1 flex h-5 gap-2"}>
               <li>
                 <TagItem tag={"foo"} size={"sm"} />
               </li>
@@ -34,10 +45,44 @@ export default function BookmarkList() {
                 <TagItem tag={"doh"} size={"sm"} />
               </li>
             </ul>
-            <div className={"border-t border-neutral-200 p-3 pb-0"}>foo</div>
+            <div
+              className={
+                "flex-center-between border-t border-neutral-200 p-1 pb-0"
+              }
+            >
+              <div className={"flex gap-2"}>
+                <OptionButton>
+                  <Star />
+                </OptionButton>
+                <OptionButton>
+                  <Share2 />
+                </OptionButton>
+              </div>
+              <OptionButton>
+                <EllipsisVertical />
+              </OptionButton>
+            </div>
           </CardContent>
         </Card>
       </div>
     </main>
+  );
+}
+
+function OptionButton({ children }: BasicComponentProps) {
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+  };
+
+  return (
+    <Button
+      size={"icon-sm"}
+      variant={"ghost"}
+      className={"text-neutral-400"}
+      onClick={handleClick}
+    >
+      {children}
+    </Button>
   );
 }
