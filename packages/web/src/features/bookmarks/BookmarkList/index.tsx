86808c937867331ed2a7665b89d4bee9d54079c8
godcl1623 <godcl1623@gmail.com>
2025-11-30 23:09:07 +0900

    feat: 디렉터리 목록 화면 초안 구현

--- packages/web/src/features/bookmarks/BookmarkList/index.tsx
+++ packages/web/src/features/bookmarks/BookmarkList/index.tsx
@@ -1,35 +1,54 @@
-import { useEffect, useState } from "react";
+import type { Bookmark, Folder as FolderType } from "@linkvault/shared";
+
+import Button from "@/shared/components/atoms/button";
+import useDirectoriesData from "@/shared/hooks/useDirectoriesData";
 
 import BlankFallback from "./components/BlankFallback";
 
 export default function BookmarkList() {
-  // const dummyList = useRandomDummyList();
+  const loadedDirectory = useDirectoriesData("/", true);
+  const { folders, bookmarks } = loadedDirectory?.data ?? {};
 
   return (
     <main className={"h-[calc(100vh-64px)] w-[85%] bg-blue-50/75 p-5"}>
-      <BlankFallback />
-      {/*{dummyList.length === 0 ? (*/}
-      {/*) : (*/}
-      {/*  <div>*/}
-      {/*    <BookmarkCard />*/}
-      {/*  </div>*/}
-      {/*)}*/}
+      {(!folders && !bookmarks) ||
+        (folders?.length === 0 && bookmarks?.length === 0 && <BlankFallback />)}
+      {folders && (
+        <article>
+          <h2>Folders</h2>
+          <ul className={"flex-center gap-2"}>
+            {folders.map((folder: FolderType) => (
+              <li key={`dir_button_${folder.data_id}`}>
+                <Button
+                  size={"custom"}
+                  variant={"ghost"}
+                  className={"rounded-lg border border-blue-300/75 px-4 py-1"}
+                >
+                  {folder.title}
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </article>
+      )}
+      {bookmarks && (
+        <article className={"mt-10"}>
+          <h2>Bookmarks</h2>
+          <ul className={"flex-center gap-2"}>
+            {bookmarks.map((bookmark: Bookmark) => (
+              <li key={`bookmark_button_${bookmark.data_id}`}>
+                <Button
+                  size={"custom"}
+                  variant={"ghost"}
+                  className={"rounded-lg border border-blue-300/75 px-4 py-1"}
+                >
+                  {bookmark.title}
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </article>
+      )}
     </main>
   );
 }
-
-const useRandomDummyList = () => {
-  const [dummyList, setDummyList] = useState<string[]>([]);
-
-  useEffect(() => {
-    const randomNumber = Math.random();
-
-    if (randomNumber < 0.5) {
-      return;
-    } else {
-      setDummyList((prev) => [...prev, "foo"]);
-    }
-  }, []);
-
-  return dummyList;
-};
