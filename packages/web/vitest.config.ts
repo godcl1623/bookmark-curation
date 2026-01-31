eef9b34ad55fc81fdd7ea592658c99cf4efda61e
godcl1623 <godcl1623@gmail.com>
2026-02-01 00:02:52 +0900

    feat: testing-library 세팅

--- packages/web/vitest.config.ts
+++ packages/web/vitest.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
     include: [
       "src/**/*.{test,spec}.{ts,tsx}",
       "src/__tests__/**/*.{test,spec}.{ts,tsx}",
