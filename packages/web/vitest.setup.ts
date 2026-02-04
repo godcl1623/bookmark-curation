fd44cc960ecd3a2ae98f6d5b06cd2a6de039c0e9
godcl1623 <godcl1623@gmail.com>
2026-02-05 02:17:56 +0900

    feat: MSW 사용을 위해 모킹 서버 인스턴스 적용 및 Provider를 적용한 render + renderHook 구현

--- packages/web/vitest.setup.ts
+++ packages/web/vitest.setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom/vitest";
+
+import { afterAll, afterEach, beforeAll } from "vitest";
+
+import { server } from "./src/__tests__/mock/node";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
