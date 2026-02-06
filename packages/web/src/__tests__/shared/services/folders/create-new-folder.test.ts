38f6f1473f56d44c39a43849c28f4e3afe073ca8
godcl1623 <godcl1623@gmail.com>
2026-02-07 01:07:00 +0900

    test: services/folder 반환값 검증 테스트 작성

---
+++ packages/web/src/__tests__/shared/services/folders/create-new-folder.test.ts
@@ -0,0 +1,15 @@
+import { EXAMPLES } from "@/__tests__/__utils__";
+import createNewFolder from "@/shared/services/folders/create-new-folder";
+
+describe("# create-new-folder 테스트", () => {
+  test("## 1. 정상 호출 테스트", async () => {
+    // act
+    const result = await createNewFolder({});
+
+    // assert
+    expect(result).toEqual({
+      ok: true,
+      data: EXAMPLES.FOLDER,
+    });
+  });
+});
---
+++ packages/web/src/__tests__/shared/services/folders/get-folders-list.test.ts
@@ -0,0 +1,15 @@
+import { EXAMPLES } from "@/__tests__/__utils__";
+import getFoldersList from "@/shared/services/folders/get-folders-list";
+
+describe("# get-folders-list 테스트", () => {
+  test("## 1. 정상 호출 테스트", async () => {
+    // act
+    const result = await getFoldersList();
+
+    // assert
+    expect(result).toEqual({
+      ok: true,
+      data: [EXAMPLES.FOLDER],
+    });
+  });
+});
