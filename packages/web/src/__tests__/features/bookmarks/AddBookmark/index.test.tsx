23ad834ab0a5e989b42a95f40db6a936aa8f0e09
godcl1623 <godcl1623@gmail.com>
2026-02-13 00:00:00 +0900

    test: AddBOokmark 테스트 설계

---
+++ packages/web/src/__tests__/features/bookmarks/AddBookmark/index.test.tsx
@@ -0,0 +1,94 @@
+describe("# AddBookmark 테스트", () => {
+  test("## 1. 기본 렌더링 테스트", async () => {
+    /* arrange */
+
+    /* act */
+
+    /* assert */
+    expect(result).toBe(expected);
+  });
+
+  describe("## 2. URL 입력 테스트", () => {
+    test("### 2-1. 일반 입력 테스트", async () => {
+      /* arrange */
+
+      /* act */
+
+      /* assert */
+      expect(result).toBe(expected);
+    });
+
+    test("### 2-2. Validation 테스트", async () => {
+      /* arrange */
+
+      /* act */
+
+      /* assert */
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe("## 3. 제목 입력 테스트", () => {
+    test("### 3-1. 일반 입력 테스트", async () => {
+      /* arrange */
+
+      /* act */
+
+      /* assert */
+      expect(result).toBe(expected);
+    });
+
+    test("### 3-2. Validation 테스트", async () => {
+      /* arrange */
+
+      /* act */
+
+      /* assert */
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe("## 4. Notes 입력 테스트", () => {
+    test("### 4-1. 일반 입력 테스트", async () => {
+      /* arrange */
+
+      /* act */
+
+      /* assert */
+      expect(result).toBe(expected);
+    });
+
+    test("### 4-2. Validation 테스트", async () => {
+      /* arrange */
+
+      /* act */
+
+      /* assert */
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe("## 5. 태그 조작 테스트", () => {
+    test("### 5-1. 일반 입력 테스트", async () => {
+      /* arrange */
+
+      /* act */
+
+      /* assert */
+      expect(result).toBe(expected);
+    });
+
+    test("### 5-2. Validation 테스트", async () => {
+      /* arrange */
+
+      /* act */
+
+      /* assert */
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe("## 6. 폼 Submit 테스트", () => {
+    /* tests */
+  });
+});
