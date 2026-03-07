1274df822c6f6c0e4b0082ba46b846fda1c0ba31
godcl1623 <godcl1623@gmail.com>
2026-03-07 15:12:11 +0900

    test: useInvalidateDirectories 테스트 추가, render 및 renderHook이 queryClient 동작 검증을 위해 외부에서 파라미터로 받을 수 있도록 수정

--- packages/web/src/__tests__/mock/utils/index.tsx
+++ packages/web/src/__tests__/mock/utils/index.tsx
@@ -9,18 +9,29 @@ import { type ReactNode, useState } from "react";
 
 import ModalProvider from "@/app/providers/ModalProvider";
 
-const renderWithProviders = (ui: ReactNode, options: RenderOptions = {}) =>
+const renderWithProviders = (
+  ui: ReactNode,
+  options: RenderOptions & { queryClient?: QueryClient } = {}
+) =>
   render(ui, {
-    wrapper: ({ children }) => <TestProvider>{children}</TestProvider>,
+    wrapper: ({ children }) => (
+      <TestProvider queryClient={options.queryClient}>{children}</TestProvider>
+    ),
     ...options,
   });
 
 const renderHookWithProviders = <Result, Props>(
   callback: (initialProps: Props) => Result,
-  renderHookOptions: RenderHookOptions<Props> = {}
+  renderHookOptions: RenderHookOptions<Props> & {
+    queryClient?: QueryClient;
+  } = {}
 ) =>
   renderHook(callback, {
-    wrapper: ({ children }) => <TestProvider>{children}</TestProvider>,
+    wrapper: ({ children }) => (
+      <TestProvider queryClient={renderHookOptions.queryClient}>
+        {children}
+      </TestProvider>
+    ),
     ...renderHookOptions,
   });
 
@@ -32,7 +43,6 @@ const createTestQueryClient = () => {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 0,
         gcTime: 0,
         retry: 0,
       },
@@ -40,11 +50,17 @@ const createTestQueryClient = () => {
   });
 };
 
-function TestProvider({ children }: { children: ReactNode }) {
-  const [queryClient] = useState(createTestQueryClient);
+function TestProvider({
+  children,
+  queryClient,
+}: {
+  children: ReactNode;
+  queryClient?: QueryClient;
+}) {
+  const [queryClientInstance] = useState(queryClient ?? createTestQueryClient);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClientInstance}>
       <ModalProvider>{children}</ModalProvider>
     </QueryClientProvider>
   );
