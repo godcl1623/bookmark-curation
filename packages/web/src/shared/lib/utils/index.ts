feb62d23eb00f609d13637e386868636aa06b6ed
godcl1623 <godcl1623@gmail.com>
2026-04-24 00:00:16 +0900

    refactor: 추가 유틸함수 구현을 위한 구조 분리

--- packages/web/src/shared/lib/utils/index.ts
+++ packages/web/src/shared/lib/utils/index.ts
@@ -1,9 +1,1 @@
-import { Capacitor } from "@capacitor/core";
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export const checkIfMobileNative = () => Capacitor.isNativePlatform();
+export * from "./styles";
