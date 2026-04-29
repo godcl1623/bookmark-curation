af81256abd8e6cf84650e5d9d094f30147aa1575
godcl1623 <godcl1623@gmail.com>
2026-04-29 22:34:30 +0900

    feat: 탐색기 트리 평탄화를 위해 전역상태 수정

--- packages/web/src/stores/global.ts
+++ packages/web/src/stores/global.ts
@@ -1,7 +1,10 @@
 import { create } from "zustand";
 
+// TODO: openIds 관련 코드 전부 삭제
+
 interface GlobalStore {
   openIds: Set<string>;
+  openPaths: Record<string, string>;
   slugToId: Record<string, string>;
   currentView: "card" | "list";
   isMobile: boolean;
@@ -10,6 +13,7 @@ interface GlobalStore {
 
 interface GlobalActions {
   toggleOpen: (id: string) => void;
+  _toggleOpen: (id: string, dirPath: string) => void;
   updateSlugToId: (tables: Record<string, string>[]) => void;
   setCurrentView: (view: "card" | "list") => void;
   setIsMobile: (isMobile: boolean) => void;
@@ -19,6 +23,7 @@ interface GlobalActions {
 
 const useGlobalStore = create<GlobalStore & GlobalActions>((set, get) => ({
   openIds: new Set(),
+  openPaths: {},
   slugToId: {},
   currentView: "card",
   isMobile: false,
@@ -38,6 +43,19 @@ const useGlobalStore = create<GlobalStore & GlobalActions>((set, get) => ({
       });
     }
   },
+  _toggleOpen: (id, dirPath) =>
+    set((state) => {
+      const next = { ...state.openPaths };
+      if (next[id]) {
+        Object.keys(next).forEach((key) => {
+          if (next[key] === dirPath || next[key].startsWith(dirPath + "/"))
+            delete next[key];
+        });
+      } else {
+        next[id] = dirPath;
+      }
+      return { openPaths: next };
+    }),
   updateSlugToId: (tables) =>
     set((state) => ({
       slugToId: {
@@ -53,6 +71,7 @@ const useGlobalStore = create<GlobalStore & GlobalActions>((set, get) => ({
   __resetStore: () =>
     set({
       openIds: new Set(),
+      openPaths: {},
       slugToId: {},
       currentView: "card",
       isMobile: false,
